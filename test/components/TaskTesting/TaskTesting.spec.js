5356a5a370ca8f8f7bb3a7b9adf0fde03b2b2e18
Philipp Fromme <philipp.fromme@camunda.com>
2025-09-26 10:37:10 +0200

    fix: fix _Test task_ button behavior
    * call configure connection callback when clicking _Test task_ and connection not configured
    
    Closes #23

--- test/components/TaskTesting/TaskTesting.spec.js
+++ test/components/TaskTesting/TaskTesting.spec.js
@@ -1,6 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import sinon from 'sinon';
 
-import { bootstrapModeler, inject } from '../../util/Util';
+import { render, screen, waitFor } from '@testing-library/react';
+
+import { bootstrapModeler, getModeler, inject } from '../../util/Util';
 
 import TaskTesting, { NO_ELEMENT_TEXT } from '../../../lib/components/TaskTesting/TaskTesting';
 
@@ -11,20 +13,112 @@ describe('TaskTesting', function() {
   beforeEach(bootstrapModeler(diagramXML));
 
 
-  it('should render', inject(function(injector) {
+  it('should render', function() {
 
     // when
-    render(<TaskTesting
-      injector={ injector }
-      deploy={ () => {} }
-      startInstance={ () => {} }
-      getInstance={ () => {} }
-      config={ {} }
-      onConfigChanged={ () => {} }
-    />);
+    renderTaskTesting();
 
     // then
     expect(screen.getByText(NO_ELEMENT_TEXT)).to.exist;
-  }));
+  });
+
+
+  describe('_Test task_ button', function() {
+
+    it('should start execution when connection configured', inject(async function(injector) {
+
+      // given
+      const spy = sinon.spy(() => Promise.resolve({ success: true, error: 'foo' }));
+
+      renderTaskTesting({
+        isConnectionConfigured: true,
+        api: {
+          deploy: spy
+        }
+      });
+
+      injector.get('selection').select(injector.get('elementRegistry').get('ServiceTask_1'));
+
+      await waitFor(() => {
+        expect(screen.getByTestId('test-task-btn').textContent).to.match(/Test task/);
+      });
+
+      // when
+      screen.getByTestId('test-task-btn').click();
+
+      // then
+      await waitFor(() => {
+        expect(spy).to.have.been.called;
+      });
+    }));
+
+
+    it('should call configure connection callback when connection not configured', inject(async function(injector) {
+
+      // given
+      const spy = sinon.spy();
+
+      renderTaskTesting({
+        isConnectionConfigured: false,
+        onConfigureConnection: spy
+      });
+
+      injector.get('selection').select(injector.get('elementRegistry').get('ServiceTask_1'));
+
+      await waitFor(() => {
+        expect(screen.getByTestId('test-task-btn').textContent).to.match(/Test task/);
+      });
+
+      // when
+      screen.getByTestId('test-task-btn').click();
+
+      // then
+      await waitFor(() => {
+        expect(spy).to.have.been.called;
+      });
+    }));
+
+  });
 
 });
+
+function renderTaskTesting(props = {}) {
+  const modeler = getModeler();
+
+  const {
+    injector = modeler.get('injector'),
+    api,
+    isConnectionConfigured,
+    configureConnectionBannerTitle = 'Connection required',
+    configureConnectionBannerDescription = 'Configure a connection to start testing.',
+    configureConnectionLabel = 'Configure',
+    onConfigureConnection,
+    config = {
+      input: {},
+      output: {}
+    },
+    onConfigChanged = () => {},
+    operateBaseUrl,
+    documentationUrl,
+    onTaskExecutionStarted = () => {},
+    onTaskExecutionFinished = () => {},
+    onTaskExecutionInterrupted = () => {}
+  } = props;
+
+  return render(<TaskTesting
+    injector={ injector }
+    api={ api }
+    isConnectionConfigured={ isConnectionConfigured }
+    configureConnectionBannerTitle={ configureConnectionBannerTitle }
+    configureConnectionBannerDescription={ configureConnectionBannerDescription }
+    configureConnectionLabel={ configureConnectionLabel }
+    onConfigureConnection={ onConfigureConnection }
+    config={ config }
+    onConfigChanged={ onConfigChanged }
+    operateBaseUrl={ operateBaseUrl }
+    documentationUrl={ documentationUrl }
+    onTaskExecutionStarted={ onTaskExecutionStarted }
+    onTaskExecutionFinished={ onTaskExecutionFinished }
+    onTaskExecutionInterrupted={ onTaskExecutionInterrupted }
+  />);
+}
