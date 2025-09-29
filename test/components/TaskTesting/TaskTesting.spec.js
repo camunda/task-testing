5ee453ca4c8a308a702c042a4b3ee48f7ca9f211
Jarek Danielak <jarek.danielak@camunda.com>
2025-09-29 14:22:35 +0200

    test(TaskTesting): add tests for task name and type; simplify

--- test/components/TaskTesting/TaskTesting.spec.js
+++ test/components/TaskTesting/TaskTesting.spec.js
@@ -1,104 +1,146 @@
-import sinon from 'sinon';
+import { render, screen } from '@testing-library/react';
 
-import { render, screen, waitFor } from '@testing-library/react';
+import { CloudElementTemplatesPropertiesProviderModule } from 'bpmn-js-element-templates';
 
-import { bootstrapModeler, getModeler, inject } from '../../util/Util';
+import {
+  BpmnPropertiesPanelModule,
+  BpmnPropertiesProviderModule,
+  ZeebePropertiesProviderModule
+} from 'bpmn-js-properties-panel';
+
+import { bootstrapModeler, inject, getModeler } from '../../util/Util';
 
 import TaskTesting from '../../../lib/components/TaskTesting/TaskTesting';
 
 import { SINGLE_TASK_SELECTION_REQUIRED_MESSAGE } from '../../../lib/hooks/useSelectedElement';
 
+import { DEFAULT_CONFIG } from '../../../lib/ElementConfig';
+
 import diagramXML from '../../fixtures/diagram.bpmn';
+import templates from '../../fixtures/elementTemplates.json';
 
 describe('TaskTesting', function() {
 
-  beforeEach(bootstrapModeler(diagramXML));
+  beforeEach(bootstrapModeler(diagramXML, {
+    additionalModules: [
+      BpmnPropertiesPanelModule,
+      BpmnPropertiesProviderModule,
+      ZeebePropertiesProviderModule,
+      CloudElementTemplatesPropertiesProviderModule
+    ]
+  }));
+
+  beforeEach(inject(function(elementTemplates) {
+    elementTemplates.set(templates);
+  }));
 
 
-  it('should render', function() {
+  it('should render', inject(function() {
 
     // when
     renderTaskTesting();
 
     // then
     expect(screen.getByText(SINGLE_TASK_SELECTION_REQUIRED_MESSAGE)).to.exist;
-  });
+  }));
+
+
+  it('should show task type and name (generic)', inject(async function(elementRegistry, selection) {
+
+    // given
+    renderTaskTesting();
+
+    // when
+    selection.select(elementRegistry.get('ServiceTask_1'));
+
+    // then
+    await screen.findByText('Script Task');
+    await screen.findByText('No inputs');
+  }));
+
+
+  it('should show task type and name (element template)', inject(async function(elementRegistry, selection) {
+
+    // given
+    renderTaskTesting();
+
+    // when
+    selection.select(elementRegistry.get('ServiceTask_3'));
+
+    // then
+    await screen.findByText('REST Outbound Connector');
+    await screen.findByText('REST');
+  }));
 
 
   describe('_Test task_ button', function() {
 
-    it('should start execution when connection configured', inject(async function(injector) {
+    it('should start execution when connection configured',
+      inject(async function(elementRegistry, selection) {
 
-      // given
-      const spy = sinon.spy(() => Promise.resolve({ success: true, error: 'foo' }));
+        // given
+        const spy = sinon.spy(() => Promise.resolve({ success: true, error: 'foo' }));
 
-      renderTaskTesting({
-        isConnectionConfigured: true,
-        api: {
-          deploy: spy
-        }
-      });
+        renderTaskTesting({
+          isConnectionConfigured: true,
+          api: {
+            deploy: spy
+          }
+        });
 
-      injector.get('selection').select(injector.get('elementRegistry').get('ServiceTask_1'));
+        selection.select(elementRegistry.get('ServiceTask_1'));
 
-      await waitFor(() => {
-        expect(screen.getByTestId('test-task-btn').textContent).to.match(/Test task/);
-      });
+        // when
+        const button = await screen.findByTestId('test-task-btn');
+        button.click();
 
-      // when
-      screen.getByTestId('test-task-btn').click();
-
-      // then
-      await waitFor(() => {
+        // then
         expect(spy).to.have.been.called;
-      });
-    }));
+      }));
 
 
-    it('should call configure connection callback when connection not configured', inject(async function(injector) {
+    it('should call configure connection callback when connection not configured',
+      inject(async function(elementRegistry, selection) {
 
-      // given
-      const spy = sinon.spy();
+        // given
+        const spy = sinon.spy();
 
-      renderTaskTesting({
-        isConnectionConfigured: false,
-        onConfigureConnection: spy
-      });
+        renderTaskTesting({
+          isConnectionConfigured: false,
+          onConfigureConnection: spy
+        });
 
-      injector.get('selection').select(injector.get('elementRegistry').get('ServiceTask_1'));
+        selection.select(elementRegistry.get('ServiceTask_1'));
 
-      await waitFor(() => {
-        expect(screen.getByTestId('test-task-btn').textContent).to.match(/Test task/);
-      });
+        const button = await screen.findByTestId('test-task-btn');
+        button.click();
 
-      // when
-      screen.getByTestId('test-task-btn').click();
-
-      // then
-      await waitFor(() => {
         expect(spy).to.have.been.called;
-      });
-    }));
+      }));
 
   });
-
 });
+
+const noopApi = {
+  deploy: () => { },
+  startInstance: () => { },
+  getInstance: () => { },
+  getProcessInstanceVariables: () => { },
+  getProcessInstanceIncident: () => { },
+};
 
 function renderTaskTesting(props = {}) {
   const modeler = getModeler();
 
   const {
     injector = modeler.get('injector'),
-    api,
+    api = noopApi,
     isConnectionConfigured,
     configureConnectionBannerTitle = 'Connection required',
     configureConnectionBannerDescription = 'Configure a connection to start testing.',
     configureConnectionLabel = 'Configure',
     onConfigureConnection,
-    config = {
-      input: {},
-      output: {}
-    },
+    config = DEFAULT_CONFIG,
     onConfigChanged = () => {},
     operateBaseUrl,
     documentationUrl,
