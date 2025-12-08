7648c1e2626eaed106c7f10b913c17f28e6f8ae0
Philipp Fromme <philipp.fromme@camunda.com>
2025-12-08 12:34:10 +0100

    fix: provide process ID explicitly
    Related to https://github.com/camunda/camunda-modeler/issues/5460

---
+++ test/utils/element.spec.js
@@ -0,0 +1,53 @@
+import { bootstrapModeler, inject } from '../util/Util';
+
+import { getProcessId } from '../../lib/utils/element';
+
+import processXML from '../fixtures/diagram.bpmn';
+import collaborationXML from '../fixtures/collaboration.bpmn';
+
+describe('element', function() {
+
+  describe('getProcessId', function() {
+
+    describe('process', function() {
+
+      beforeEach(bootstrapModeler(processXML));
+
+
+      it('should return process ID (process)', inject(function(elementRegistry) {
+
+        // given
+        const element = elementRegistry.get('ServiceTask_1');
+
+        // when
+        const processId = getProcessId(element);
+
+        // then
+        expect(processId).to.equal('Process_TaskTesting');
+      }));
+
+    });
+
+
+    describe('collaboration', function() {
+
+      beforeEach(bootstrapModeler(collaborationXML));
+
+
+      it('should return process ID (collaboration)', inject(function(elementRegistry) {
+
+        // given
+        const element = elementRegistry.get('ServiceTask_1');
+
+        // when
+        const processId = getProcessId(element);
+
+        // then
+        expect(processId).to.equal('Process_TaskTesting');
+      }));
+
+    });
+
+  });
+
+});
