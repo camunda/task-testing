e47ea1717bd150be7928de7ea57070d1640dbd2e
Anton von Weltzien <anton.weltzien@camunda.com>
2026-02-10 17:59:14 +0100

    feat(types): update types to orchestration-cluster-api types
    * add type additional type checks in TaskExecution.js

--- lib/types.ts
+++ lib/types.ts
@@ -1,114 +1,135 @@
 import type {
-  CreateProcessInstanceResponse,
-  DeployResourceResponse,
-  SearchProcessInstanceResponse,
-  SearchVariablesResponse,
-  SearchElementInstancesResponse,
-  SearchIncidentsResponse
-} from '@camunda8/sdk/dist/c8/lib/C8Dto';
+    CamundaClient,
+    CreateProcessInstanceResult,
+    ElementInstanceResult,
+    ElementInstanceSearchQueryResult,
+    IncidentResult,
+    IncidentSearchQueryResult,
+    ProcessInstanceResult,
+    ProcessInstanceSearchQueryResult,
+    ProcessInstanceStateEnum,
+    VariableSearchQueryResult,
+    VariableSearchResult
+} from '@camunda8/orchestration-cluster-api';
+import type {ModdleElement} from "bpmn-js/lib/model/Types";
 
-import type {
-  ModdleElement
-} from "bpmn-js/lib/model/Types";
+declare type ExtendedDeploymentResult = Awaited<ReturnType<CamundaClient['createDeployment']>>
+
+export type {
+    CreateProcessInstanceResult,
+    ElementInstanceSearchQueryResult,
+    IncidentSearchQueryResult,
+    ProcessInstanceSearchQueryResult,
+    ProcessInstanceResult,
+    VariableSearchQueryResult,
+    ExtendedDeploymentResult,
+    ProcessInstanceStateEnum,
+    VariableSearchResult,
+    ElementInstanceResult,
+    IncidentResult
+}
+
 
 export type Input = {
-  [elementId: string]: string;
+    [elementId: string]: string;
 };
 
 export type Output = {
-  [elementId: string]: ElementOutput
+    [elementId: string]: ElementOutput
 };
 
 export type VARIABLE_SCOPE = 'LOCAL' | 'PROCESS';
 
 export type ElementOutputVariables = {
-  [id: string]: {
-    name: string;
-    value: any;
-    scope: VARIABLE_SCOPE;
-  }
+    [id: string]: {
+        name: string;
+        value: any;
+        scope: VARIABLE_SCOPE;
+    }
 };
 
 export type ElementOutput = {
-  success: boolean;
-  variables?: ElementOutputVariables;
-  error?: TaskExecutionError;
-  incident?: any;
-  operateUrl?: string;
-  [key: string]: any;
+    success: boolean;
+    variables?: ElementOutputVariables;
+    error?: TaskExecutionError;
+    incident?: any;
+    operateUrl?: string;
+    [key: string]: any;
 } | undefined;
 
 export type Config = {
-  input: Input;
-  output: Output;
+    input: Input;
+    output: Output;
 };
 
 export type Variable = {
-  name: string;
-  type?: string;
-  info?: string;
-  isList?: boolean;
-  entries?: Variable[];
-  scope?: ModdleElement;
+    name: string;
+    type?: string;
+    info?: string;
+    isList?: boolean;
+    entries?: Variable[];
+    scope?: ModdleElement;
 };
 
 export type Variables = Variable[];
 
 export type ApiResponse<T> =
-  | {
-      success: true;
-      response: T;
-    }
-  | {
-      success: false;
-      error: string;
-    }
+    | {
+    success: true;
+    response: T;
+}
+    | {
+    success: false;
+    error: string;
+}
 
 export type TaskExecutionApi = {
-  deploy: () => Promise<ApiResponse<DeployResourceResponse>>;
-  startInstance: (processDefinitionKey: string, elementId: string, variables: { [key: string]: any }) => Promise<ApiResponse<CreateProcessInstanceResponse>>;
-  getProcessInstance: (processInstanceKey: string) => Promise<ApiResponse<SearchProcessInstanceResponse>>;
-  getProcessInstanceVariables: (processInstanceKey: string) => Promise<ApiResponse<SearchVariablesResponse>>;
-  getProcessInstanceElementInstances: (processInstanceKey: string) => Promise<ApiResponse<SearchElementInstancesResponse>>;
-  getProcessInstanceIncident: (processInstanceKey: string) => Promise<ApiResponse<SearchIncidentsResponse>>;
+    deploy: () => Promise<ApiResponse<ExtendedDeploymentResult>>;
+    startInstance: (processDefinitionKey: string, elementId: string, variables: {
+        [key: string]: any
+    }) => Promise<ApiResponse<CreateProcessInstanceResult>>;
+    getProcessInstance: (processInstanceKey: string) => Promise<ApiResponse<ProcessInstanceSearchQueryResult>>;
+    getProcessInstanceVariables: (processInstanceKey: string) => Promise<ApiResponse<VariableSearchQueryResult>>;
+    getProcessInstanceElementInstances: (processInstanceKey: string) => Promise<ApiResponse<ElementInstanceSearchQueryResult>>;
+    getProcessInstanceIncident: (processInstanceKey: string) => Promise<ApiResponse<IncidentSearchQueryResult>>;
 };
 
 export type TaskExecutionStatus =
-  'idle' |
-  'deploying' |
-  'starting-instance' |
-  'executing';
+    'idle' |
+    'deploying' |
+    'starting-instance' |
+    'executing';
 
 export type TaskExecutionEvents =
-  'taskExecution.status.changed' |
-  'taskExecution.finished' |
-  'taskExecution.error' |
-  'taskExecution.interrupted';
+    'taskExecution.status.changed' |
+    'taskExecution.finished' |
+    'taskExecution.error' |
+    'taskExecution.interrupted';
 
 export type TaskExecutionResult = {
-  success: boolean;
-  variables?: ElementOutputVariables;
-  error?: TaskExecutionError;
-  incident?: any;
+    success: boolean;
+    variables?: ElementOutputVariables;
+    error?: TaskExecutionError;
+    incident?: any;
 }
 
 export type TaskExecutionError = {
-  message: string;
-  response?: any;
+    message: string;
+    response?: any;
 };
 
-export type { Element, ModdleElement } from 'bpmn-js/lib/model/Types';
+export type {Element, ModdleElement} from 'bpmn-js/lib/model/Types';
 
 export type Plugin = {
-  priority?: number;
-  render: Function;
-  type: string;
-  [key: string]: any;
+    priority?: number;
+    render: Function;
+    type: string;
+    [key: string]: any;
 };
 
 export type PluginContextValue = {
-  plugins: Plugin[];
-  registerPlugin: (plugin: Plugin) => void;
-  unregisterPlugin: (plugin: Plugin) => void;
-  getPlugins: (pluginPoint: string) => Plugin[];
+    plugins: Plugin[];
+    registerPlugin: (plugin: Plugin) => void;
+    unregisterPlugin: (plugin: Plugin) => void;
+    getPlugins: (pluginPoint: string) => Plugin[];
 };
