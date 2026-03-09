e93bb5b740e1f47b2ddac1c545ff0653977099b4
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-03-09 20:01:34 +0100

    chore: use puppeteer provided Chrome

--- karma.config.js
+++ karma.config.js
@@ -2,6 +2,9 @@
 // any of [ 'ChromeHeadless', 'Chrome', 'Firefox' ]
 const browsers = (process.env.TEST_BROWSERS || 'ChromeHeadless').split(',');
 
+// use puppeteer provided Chrome for testing
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function(config) {
   config.set({
     frameworks: [
